cad86948566ec55c366c94602a27ed14025d67b1
Muhammad Mustafa <muhamed2112@gmail.com>
2023-01-21 23:39:00 +0200

    exclude seq and NM from tsc

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+// @ts-ignore
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
